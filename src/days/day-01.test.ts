5710fa579cbd49858e24564851dcfd6d3a1cf37d
Menno Pruijssers <menno@originalsuperman.nl>
2024-12-04 22:15:23 +0100

    day 1

---
+++ src/days/day-01.test.ts
@@ -0,0 +1,14 @@
+import Day from './day-01';
+import { dayRunner, dayVerifier } from './test-util';
+
+const example = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
+
+dayRunner(Day, example, 11, 31);
+
+
+dayVerifier(1, 1151792, 21790168);
---
+++ src/days/day-01.ts
@@ -0,0 +1,42 @@
+import { BaseDay } from '../day';
+
+type Input = [number[], number[]];
+export class Day extends BaseDay<Input, number, number> {
+  parse(input: string): Input {
+    const lines = input.split('\n').map((line) => line.split(/ +/).map(Number));
+
+    const left = lines.map((line) => line[0]);
+    const right = lines.map((line) => line[1]);
+
+    return [left, right];
+  }
+
+  async partOne(): Promise<number> {
+    const left = this.input[0].sort((a, b) => a - b);
+    const right = this.input[1].sort((a, b) => a - b);
+
+    const differences = left.map((n, i) => Math.abs(n - right[i]));
+    return differences.reduce((sum, n) => sum + n);
+  }
+
+  async partTwo(): Promise<number> {
+    const [left, right] = this.input;
+
+    const rightMap = right.reduce<Record<number, number>>((map, n) => {
+      if (map[n]) {
+        map[n]++;
+      } else {
+        map[n] = 1;
+      }
+      return map;
+    }, {});
+
+    return left.reduce((sum, n) => {
+      const count = rightMap[n] ?? 0;
+      const score = count * n;
+      return sum + score;
+    }, 0);
+  }
+}
+
+export default Day;
